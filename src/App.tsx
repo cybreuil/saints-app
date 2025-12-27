cbdc4b356d6dbc9de8965ee7a1ee985638b38658
Cyrille Breuil <cybreuil@gmail.com>
2025-12-27 22:53:31 +0100

    Initi with First saint pic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div className="presentation">
 				<h1>Saints-App</h1>
 				<p>React App for catholic saints information.</p>
+				<img
+					className="logo"
+					src="/saints-pic/Laurent.jpg"
+					alt="Saint Laurent"
+				/>
 			</div>
 		</>
 	);
