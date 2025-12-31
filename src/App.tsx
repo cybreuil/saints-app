8e49763db899a9813e03b0eef4fe0a2e55df6aff
Cyrille Breuil <cybreuil@gmail.com>
2025-12-31 13:04:59 +0100

    improvement and new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import { Logo } from "./components/Logo/Logo";
 import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
 	return (
 		<>
 			<div className="presentation">
-				<img src="/logo.svg" />
+				<Logo color="brown" />
 				<h1>Saints-App</h1>
 				<p>React App for catholic saints information.</p>
 				<Link className="link-today" to="/saint-of-the-day">
