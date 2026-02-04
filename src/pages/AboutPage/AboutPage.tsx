d0d7bd43130be19b2992b6f9b834e77369751ef6
Cyrille Breuil <cybreuil@gmail.com>
2026-02-04 23:26:42 +0100

    upgrade display aboutpage

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,21 +4,23 @@ const AboutPage = () => {
 	return (
 		<div className="about-page">
 			<h1>About Saints-App</h1>
-			<p>
-				Saints-App is dedicated to providing users with daily
-				information about saints from various traditions. Our mission is
-				to educate and inspire individuals by sharing the stories and
-				legacies of these remarkable figures.
-			</p>
-			<p>
-				This application was developed with love and care to ensure a
-				user-friendly experience. We hope you find value in the content
-				and enjoy exploring the lives of saints.
-			</p>
-			<p>
-				For any questions or feedback, please feel free to reach out to
-				us.
-			</p>
+			<div className="about-page__info">
+				<p>
+					Saints-App is dedicated to providing users with daily
+					information about saints from various traditions. Our
+					mission is to educate and inspire individuals by sharing the
+					stories and legacies of these remarkable figures.
+				</p>
+				<p>
+					This application was developed with love and care to ensure
+					a user-friendly experience. We hope you find value in the
+					content and enjoy exploring the lives of saints.
+				</p>
+				<p>
+					For any questions or feedback, please feel free to reach out
+					to us.
+				</p>
+			</div>
 		</div>
 	);
 };
