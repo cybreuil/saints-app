3fed7243f50cea07f3d7f218c2e17f7ae0ad60ff
Cyrille Breuil <cybreuil@gmail.com>
2026-02-06 16:45:38 +0100

    feat: animate header clean

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,34 @@ import { Logo } from "../Logo/Logo";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { TRANSITIONS } from "../../styles/theme";
+import { useState, useEffect } from "react";
 import "./Header.css";
 
 const Header = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	// On ecoute le scroll pour ajouter une classe "scrolled" au header lorsque l'utilisateur a scrollé de plus de 50px
+	useEffect(() => {
+		const handleScroll = () => {
+			setIsScrolled(window.scrollY > 50);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
 		<motion.header
 			className="header"
 			initial={{ y: -50, opacity: 0 }}
-			animate={{ y: 0, opacity: 1 }}
+			animate={{
+				y: 0,
+				opacity: 1,
+				width: isScrolled ? "100%" : "90%",
+				marginTop: isScrolled ? 0 : "1rem",
+				borderRadius: isScrolled ? 0 : "1rem",
+				top: isScrolled ? 0 : "1rem",
+			}}
 			transition={TRANSITIONS.normal}
 		>
 			<Logo />
