a0a8c7e3153c614a2173a55cb9e3940693bc1b23
Cyrille Breuil <cybreuil@gmail.com>
2026-02-06 18:55:07 +0100

    huge header display progress

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -43,49 +43,101 @@ const Header = () => {
 				width: isScrolled ? "100%" : "90%",
 				borderRadius: isScrolled ? 0 : "1rem",
 				top: isScrolled ? 0 : "1rem",
-				height: isMenuOpen ? "80vh" : "",
+				height: isMenuOpen ? "300px" : "",
 			}}
 			transition={TRANSITIONS.normal}
 		>
-			<Logo />
-			<h1 className="header-title">Saints-App</h1>
-			{isScrolled ? (
-				<nav className="header-nav">
-					<Link to="/">Home</Link>
-					<Link to="/saint-of-the-day">Feast of the Day</Link>
-					<Link to="/saints">Saints List</Link>
-					<Link to="/search">Search</Link>
-					<Link to="/about">About</Link>
-				</nav>
-			) : (
-				<BurgerIcon
-					isOpen={isMenuOpen}
-					onClick={() => setIsMenuOpen((prev) => !prev)}
-				/>
-			)}
+			<div className="header__main">
+				<Logo />
+				<h1 className="header-title">Saints-App</h1>
+				{isScrolled ? (
+					<nav className="header-nav">
+						<Link to="/">Home</Link>
+						<Link to="/saint-of-the-day">Feast of the Day</Link>
+						<Link to="/saints">Saints List</Link>
+						<Link to="/search">Search</Link>
+						<Link to="/about">About</Link>
+					</nav>
+				) : (
+					<BurgerIcon
+						isOpen={isMenuOpen}
+						onClick={() => setIsMenuOpen((prev) => !prev)}
+					/>
+				)}
+			</div>
 			{isMenuOpen && (
-				<nav className="header-nav mobile">
-					<Link to="/" onClick={() => setIsMenuOpen(false)}>
-						Home
-					</Link>
-					<Link
-						to="/saint-of-the-day"
-						onClick={() => setIsMenuOpen(false)}
-					>
-						Feast of the Day
-					</Link>
-					<Link to="/saints" onClick={() => setIsMenuOpen(false)}>
-						Saints List
-					</Link>
-					<Link to="/search" onClick={() => setIsMenuOpen(false)}>
-						Search
-					</Link>
-					<Link to="/about" onClick={() => setIsMenuOpen(false)}>
-						About
-					</Link>
-				</nav>
+				<motion.nav
+					className="header-nav__burger"
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+				>
+					<div className="header-nav__burger-column">
+						<Link to="/" onClick={() => setIsMenuOpen(false)}>
+							Home
+						</Link>
+						<Link
+							to="/saint-of-the-day"
+							onClick={() => setIsMenuOpen(false)}
+						>
+							Feast of the Day
+						</Link>
+					</div>
+					<div className="header-nav__burger-column">
+						<h3>Saints</h3>
+						<ul>
+							<li>
+								<Link
+									to="/saints"
+									onClick={() => setIsMenuOpen(false)}
+								>
+									Saints List
+								</Link>
+							</li>
+							<li>
+								<Link
+									to="/search"
+									onClick={() => setIsMenuOpen(false)}
+								>
+									Search
+								</Link>
+							</li>
+							<li>
+								<Link
+									to="/about"
+									onClick={() => setIsMenuOpen(false)}
+								>
+									About
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="header-nav__burger-column">
+						<h3>Contact</h3>
+						{/*Mail / github */}
+						<ul>
+							<li>
+								<Link
+									to="https://github.com/cybreuil"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Github
+								</Link>
+							</li>
+							<li>
+								<Link
+									to="mailto:cybreuil@gmail.com"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Contact
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</motion.nav>
 			)}
-			)
 		</motion.header>
 	);
 };
