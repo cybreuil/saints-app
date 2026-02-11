4c5394924d894a734680359a0462a9bf304501ce
Cyrille Breuil <cybreuil@gmail.com>
2026-02-11 23:53:58 +0100

    refactor appRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { SaintOfTheDay } from "./pages/SaintOfTheDay/SaintOfTheDay";
-import { SaintsPage } from "./pages/SaintsPage/SaintsPage";
-import { AboutPage } from "./pages/AboutPage/AboutPage";
 import { BodyClassController } from "./components/BodyClassController/BodyClassController";
-import { SearchPage } from "./pages/SearchPage/SearchPage";
 import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
+import { AppRoutes } from "./components/AppRoutes/AppRoutes";
 
 function App() {
 	return (
@@ -15,18 +10,7 @@ function App() {
 			<ScrollToTop />
 			<Header />
 			<BodyClassController />
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/saint-of-the-day" element={<SaintOfTheDay />} />
-				<Route
-					path="/saint-of-the-day/:date"
-					element={<SaintOfTheDay />}
-				/>
-				<Route path="/saints" element={<SaintsPage />} />
-				<Route path="/search" element={<SearchPage />} />
-				<Route path="/about" element={<AboutPage />} />
-				<Route path="*" element={<HomePage />} />
-			</Routes>
+			<AppRoutes />
 			<Footer />
 		</>
 	);
---
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../../pages/HomePage/HomePage";
+import { SaintOfTheDay } from "../../pages/SaintOfTheDay/SaintOfTheDay";
+import { SaintsPage } from "../../pages/SaintsPage/SaintsPage";
+import { SearchPage } from "../../pages/SearchPage/SearchPage";
+import { AboutPage } from "../../pages/AboutPage/AboutPage";
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/saint-of-the-day" element={<SaintOfTheDay />} />
+			<Route path="/saint-of-the-day/:date" element={<SaintOfTheDay />} />
+			<Route path="/saints" element={<SaintsPage />} />
+			<Route path="/search" element={<SearchPage />} />
+			<Route path="/about" element={<AboutPage />} />
+			<Route path="*" element={<HomePage />} />
+		</Routes>
+	);
+};
+
+export { AppRoutes };
