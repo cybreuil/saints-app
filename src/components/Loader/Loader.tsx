ef51b95ab1726203abb0ca11af9f10c2ad5fcfed
Cyrille Breuil <cybreuil@gmail.com>
2026-02-20 17:39:47 +0100

    loader done

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -6,11 +6,13 @@ interface LoaderProps {
 }
 
 const Loader = ({ size = 80 }: LoaderProps) => {
+	const borderWidth = `${size * 0.05}px`; // 5% of the size for the border width
+
 	return (
 		<div className="loader-container">
 			<div className={`lds-ripple`} style={{ width: size, height: size }}>
-				<div></div>
-				<div></div>
+				<div style={{ borderWidth }}></div>
+				<div style={{ borderWidth }}></div>
 			</div>
 		</div>
 	);
