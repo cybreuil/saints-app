899708d9d0ade9e22a7cc1498a500d805f7144ff
Cyrille Breuil <cybreuil@gmail.com>
2026-02-25 01:47:13 +0100

    wrapper for modal to keep scroll inside

--- src/components/SaintModal/SaintModal.tsx
+++ src/components/SaintModal/SaintModal.tsx
@@ -57,95 +57,101 @@ export function SaintModal({
 				animate={{ opacity: 1 }}
 				transition={TRANSITIONS.normal}
 			>
-				<div className="saint-modal__left">
-					<motion.div
-						className="saint-modal__left__top"
-						initial={{ opacity: 0, x: -20 }}
-						animate={{ opacity: 1, x: 0 }}
-						transition={{ ...TRANSITIONS.slower }}
-					>
-						<button
-							className="saint-modal__close"
-							onClick={onClose}
+				<div className="saint-modal-layout">
+					<div className="saint-modal__left">
+						<motion.div
+							className="saint-modal__left__top"
+							initial={{ opacity: 0, x: -20 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ ...TRANSITIONS.slower }}
 						>
-							← Retour
-						</button>
-					</motion.div>
-					<motion.img
-						layoutId={`saint-img-${saint.id}`}
-						src={saint.image || "/logoOptimized.svg"}
-						alt={saint.name}
-						className="saint-modal__img"
-						transition={TRANSITIONS.normal}
-					/>
-					<motion.div
-						className="saint-modal__left__bottom"
-						initial={{ opacity: 0, x: -20 }}
-						animate={{ opacity: 1, x: 0 }}
-						transition={TRANSITIONS.slower}
-					>
-						<RippleButton
-							className="link-button"
-							onClick={() => alert("Voir la fiche complète")}
+							<button
+								className="saint-modal__close"
+								onClick={onClose}
+							>
+								← Retour
+							</button>
+						</motion.div>
+						<motion.img
+							layoutId={`saint-img-${saint.id}`}
+							src={saint.image || "/logoOptimized.svg"}
+							alt={saint.name}
+							className="saint-modal__img"
+							transition={TRANSITIONS.normal}
+						/>
+						<motion.div
+							className="saint-modal__left__bottom"
+							initial={{ opacity: 0, x: -20 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={TRANSITIONS.slower}
 						>
-							{saint.sex === "female"
-								? "Voir la fête de cette sainte"
-								: "Voir la fête de ce saint"}
-						</RippleButton>
-						<p className="saint-modal__id">ID : {saint.id}</p>
-					</motion.div>
-				</div>
-				<motion.div
-					className="saint-modal__content"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={TRANSITIONS.slower}
-				>
-					<motion.h2
-						layoutId={`saint-name-${saint.id}`}
-						transition={TRANSITIONS.normal}
-					>
-						{saint.name}
-					</motion.h2>
-					<motion.p
-						className="saint-modal__content__date"
-						layoutId={`saint-feastDay-${saint.id}`}
-						transition={TRANSITIONS.normal}
-					>
-						{saint.feastDay}
-					</motion.p>
-					{saint.description && (
-						<p className="saint-modal__content__description">
-							{saint.description}
-						</p>
-					)}
-					<div className="saint-modal__content__columns">
-						{saint.attributes && (
-							<p className="saint-modal__content__attributes">
-								Attributs :{" "}
-								<ul>
-									{saint.attributes
-										.split(",")
-										.map((attr, index) => (
-											<li key={index}>{attr.trim()}</li>
-										))}
-								</ul>
-							</p>
-						)}
-						{saint.patronage && (
-							<p className="saint-modal__content__patronage">
-								Patronage :{" "}
-								<ul>
-									{saint.patronage
-										.split(",")
-										.map((pat, index) => (
-											<li key={index}>{pat.trim()}</li>
-										))}
-								</ul>
-							</p>
-						)}
+							<RippleButton
+								className="link-button"
+								onClick={() => alert("Voir la fiche complète")}
+							>
+								{saint.sex === "female"
+									? "Voir la fête de cette sainte"
+									: "Voir la fête de ce saint"}
+							</RippleButton>
+							<p className="saint-modal__id">ID : {saint.id}</p>
+						</motion.div>
 					</div>
-				</motion.div>
+					<div className="saint-modal__content">
+						<motion.h2
+							layoutId={`saint-name-${saint.id}`}
+							transition={TRANSITIONS.normal}
+						>
+							{saint.name}
+						</motion.h2>
+						<motion.p
+							className="saint-modal__content__date"
+							layoutId={`saint-feastDay-${saint.id}`}
+							transition={TRANSITIONS.normal}
+						>
+							{saint.feastDay}
+						</motion.p>
+						{saint.description && (
+							<motion.p
+								className="saint-modal__content__description"
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								transition={TRANSITIONS.slower}
+							>
+								{saint.description}
+							</motion.p>
+						)}
+						<div className="saint-modal__content__columns">
+							{saint.attributes && (
+								<p className="saint-modal__content__attributes">
+									Attributs :{" "}
+									<ul>
+										{saint.attributes
+											.split(",")
+											.map((attr, index) => (
+												<li key={index}>
+													{attr.trim()}
+												</li>
+											))}
+									</ul>
+								</p>
+							)}
+							{saint.patronage && (
+								<p className="saint-modal__content__patronage">
+									Patronage :{" "}
+									<ul>
+										{saint.patronage
+											.split(",")
+											.map((pat, index) => (
+												<li key={index}>
+													{pat.trim()}
+												</li>
+											))}
+									</ul>
+								</p>
+							)}
+						</div>
+					</div>
+				</div>
 			</motion.div>
 		</>,
 		document.body,
