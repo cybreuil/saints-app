dd150346f8f8a1d3dce273e0b680b62842488798
Cyrille Breuil <cybreuil@gmail.com>
2026-06-10 00:00:03 +0200

    added env

---
+++ src/hooks/useSaints.ts
@@ -0,0 +1,14 @@
+import type { SaintsApiResponse } from "../types/saint";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:8080";
+
+const useSaints = () => {
+	const getSaints = async (page: number): Promise<SaintsApiResponse> => {
+		const response = await fetch(`${API_BASE_URL}/saints?page=${page}`);
+		return await response.json();
+	};
+
+	return { getSaints };
+};
+
+export { useSaints };
