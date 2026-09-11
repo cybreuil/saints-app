4297e091b49ac83a53cd92941d945e9fef759537
Cyrille Breuil <cybreuil@gmail.com>
2026-09-11 20:40:05 +0200

    searching images

---
+++ src/assets/altars.ts
@@ -0,0 +1,12 @@
+export const ALTAR_BY_COLOR: Record<string, string> = {
+	GREEN: "/images/altars/green-altar.jpg",
+	WHITE: "/images/altars/white-altar.jpg",
+	RED: "/images/altars/red-altar.jpg",
+	BLUE: "/images/altars/blue-altar.jpg",
+	BLACK: "/images/altars/black-altar.jpg",
+	VIOLENT: "/images/altars/violent-altar.jpg",
+	ROSE: "/images/altars/rose-altar.jpg",
+	// Add more colors and their corresponding altars as needed
+};
+
+export const ALTAR_DEFAULT = "/images/altars/default-altar.jpg";
